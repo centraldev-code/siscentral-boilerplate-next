b62de75ba312ee123ed5a5cee7362898747059c7
Central Dev <centraldev@outlook.com.br>
2024-05-19 23:18:51 -0300

    chore: initial jest tests

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>Meu Component</h1>
+    </main>
+  )
+}
+
+export default Main
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    //render component
+    const { container } = render(<Main />)
+    //Search element is verifier the exist
+    expect(
+      screen.getByRole('heading', { name: /Meu Component/i })
+    ).toBeInTheDocument()
+    //generate snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
